e942f131a9d473959dd5fb4cb7755500e09f7aec
Rodrigo Miranda <rodrigomiranda@Rodrigo5500X.localdomain>
2021-05-21 18:49:06 -0300

    Add Home styles, mocks and tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-import Home from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+import Home, { HomeTemplateProps } from 'templates/Home'
 
-export default function Index() {
-  return <Home />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+// getStaticProps => gerar estatico em build time
+// getServerSideProps => gerar via ssr a cada request
+// getInitialProps => gerar via ssr a cada request
+export function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados de uma API
+
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcomingGames: gamesMock,
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighlight: highlightMock
+    }
+  }
 }
